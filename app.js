9e90e5c8e9482f708f2fb7da7536c630efd73af4
Antje <antje.som@gmail.com>
2019-08-29 16:00:00 +0200

    create password form component

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+Vue.component('password-form', {
+    template: `
+    <form @submit.prevent="onSubmit">
+      <p>
+        <label for="name">Name/E-mail:</label>
+        <input id="name" v-model="name" placeholder="name or email">
+      </p>
+      <p>
+        <label for="name">Password</label>
+        <input id="password" v-model="password" placeholder="password">
+      </p>
+    </form>
+  `,
+    data() {
+        return {
+            name: null,
+            password: null
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+        message: 'Hello Vue!'
     }
-  })
+})
