573d01ddfaf6e6e79b321eadb7a0c35e447b0acb
Antje <antje.som@gmail.com>
2019-09-19 15:56:06 +0200

    refactor css and app.js

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 Vue.component('password-form', {
     template: `
     <form @submit.prevent="onSubmit">
-      <p>
-        <label for="username">Name/E-mail:</label>
-        <input  type="text" id="name" v-model="username" placeholder="name or email">
-      </p>
-      <p>
-        <label for="name">Password</label>
-        <input type="password" id="password" v-model="password" placeholder="password">
-      </p>
-      <p>
-        <input type="submit" value="submit">
-      </p>
+      <h1>Vue Password Strength Indicator</h1>
+      <label class="frmLabel" for="password">Password</label>
+      <input placeholder="Enter your password" name="password" class="frmField" type="password" />
+      <p class="frmValidation">Longer than 7 characters</p>
+      <p class="frmValidation">Has a capital letter</p>
+      <p class="frmValidation">Has a lowercase letter</p>
+      <p class="frmValidation">Has a number</p>
+      <p class="frmValidation">Has a special character</p>
     </form>
-    <password-validation></password-validation>
   `,
     data() {
         return {
@@ -23,29 +19,6 @@ Vue.component('password-form', {
     }
 })
 
-Vue.component('password-validation', {
-    template: `
-      <ol>
-        <li v-for="requirement in requirements">
-          {{requirement.text}}
-        </li>
-      </ol>
-  `,
-    data() {
-        return {
-            requirements: [
-                { text: 'A <b>lowercase letter</b>' },
-                { text: 'A <b>capital (uppercase)</b> ' },
-                { text: 'Minimum <b>8 characters</b>' },
-                { text: 'A <b>number or special character</b>' },
-            ]
-        }
-    }
-})
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
+app = new Vue({
+    el: '#app'
+});
