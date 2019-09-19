87155fed19b1c56b06bee876528c76b1a5265acf
Antje <antje.som@gmail.com>
2019-09-19 17:46:49 +0200

    refactor methods in form component

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ Vue.component('password-form', {
       <h1>Vue Password Strength Validation</h1>
       <label class="frmLabel" for="password">Password</label>
       <input placeholder="Enter your password" name="password" class="frmField" type="password" @input="password_check" v-model="message"/>
+      <i @click="password_toggle" class="fas fa-eye"></i>
       <p class="frmValidation" :class="{'frmValidation--passed' :message.length > 7}"><i class="frmIcon fas" :class="message.length > 7 ? 'fa-check' : 'fa-times'"></i> Longer than 7 characters</p>
       <p class="frmValidation" :class="{'frmValidation--passed' :has_uppercase }"><i class="frmIcon fas" :class="has_uppercase ? 'fa-check' : 'fa-times'"></i> Has a capital letter</p>
       <p class="frmValidation" :class="{'frmValidation--passed' :has_lowercase }"><i class="frmIcon fas" :class="has_lowercase ? 'fa-check' : 'fa-times'"></i> Has a lowercase letter</p>
@@ -17,15 +18,19 @@ Vue.component('password-form', {
             has_number: false,
             has_lowercase: false,
             has_uppercase: false,
-            has_special: false
+            has_special: false,
+            show_password: false
         }
     },
     methods: {
-        password_check: function() {
+        password_check() {
             this.has_number = /\d/.test(this.message);
             this.has_lowercase = /[a-z]/.test(this.message);
             this.has_uppercase = /[A-Z]/.test(this.message);
             this.has_special = /[!@#\$%\^\&*\)\(+=._-]/.test(this.message);
+        },
+        password_toggle() {
+            //pwd toggle goes here
         }
     }
 })
