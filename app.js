48a99ee16184bb4387d5dd62b3e8727bb9a1b4d5
Antje <antje.som@gmail.com>
2019-09-20 14:06:16 +0200

    refactor naming in password component

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ Vue.component('password-form', {
     <form @submit.prevent="onSubmit">
       <h1>Vue Password Strength Validation</h1>
       <label class="frmLabel" for="password">Password</label>
-      <input placeholder="Enter your password" name="password" class="frmField" type="password" @input="password_check" v-model="message"/>
+      <input placeholder="Enter your password" name="password" class="frmField" type="password" @input="password_check" v-model="password"/>
       <i @click="password_toggle" class="fas fa-eye"></i>
-      <p class="frmValidation" :class="{'frmValidation--passed' :message.length > 7}"><i class="frmIcon fas" :class="message.length > 7 ? 'fa-check' : 'fa-times'"></i> Longer than 7 characters</p>
+      <p class="frmValidation" :class="{'frmValidation--passed' :password.length > 7}"><i class="frmIcon fas" :class="password.length > 7 ? 'fa-check' : 'fa-times'"></i> Longer than 7 characters</p>
       <p class="frmValidation" :class="{'frmValidation--passed' :has_uppercase }"><i class="frmIcon fas" :class="has_uppercase ? 'fa-check' : 'fa-times'"></i> Has a capital letter</p>
       <p class="frmValidation" :class="{'frmValidation--passed' :has_lowercase }"><i class="frmIcon fas" :class="has_lowercase ? 'fa-check' : 'fa-times'"></i> Has a lowercase letter</p>
       <p class="frmValidation" :class="{'frmValidation--passed' :has_number }"><i class="frmIcon fas" :class="has_number ? 'fa-check' : 'fa-times'"></i> Has a number</p>
@@ -14,7 +14,7 @@ Vue.component('password-form', {
   `,
     data() {
         return {
-            message: '',
+            password: '',
             has_number: false,
             has_lowercase: false,
             has_uppercase: false,
@@ -24,13 +24,13 @@ Vue.component('password-form', {
     },
     methods: {
         password_check() {
-            this.has_number = /\d/.test(this.message);
-            this.has_lowercase = /[a-z]/.test(this.message);
-            this.has_uppercase = /[A-Z]/.test(this.message);
-            this.has_special = /[!@#\$%\^\&*\)\(+=._-]/.test(this.message);
+            this.has_number = /\d/.test(this.password);
+            this.has_lowercase = /[a-z]/.test(this.password);
+            this.has_uppercase = /[A-Z]/.test(this.password);
+            this.has_special = /[!@#\$%\^\&*\)\(+=._-]/.test(this.password);
         },
         password_toggle() {
-            //pwd toggle goes here
+
         }
     }
 })
